7fd79781d470d431a8a74f91f59e65547a516ad8
RishadIslam2400 <mds222@lehigh.edu>
2026-04-18 02:46:22 -0400

    added tcp_latency opt

--- tcp_latency.cc
+++ tcp_latency.cc
@@ -61,11 +61,11 @@ void run_tcp_server() {
   }
 
   // Optmization 2: Disable delayed ACKs
-  /* if (setsockopt(server_fd, IPPROTO_TCP, TCP_QUICKACK, &opt, sizeof(opt)) < 0) {
+  if (setsockopt(server_fd, IPPROTO_TCP, TCP_QUICKACK, &opt, sizeof(opt)) < 0) {
     log_error("setsockopt(TCP_NODELAY) failed:", errno);
     close(server_fd);
     return;
-  } */
+  }
 
   // create the address to bind with server socket
   sockaddr_in address{};
@@ -138,6 +138,14 @@ void run_tcp_client(const char* ip) {
 
   if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
     std::cerr << "TCP Connection Failed\n";
+    return;
+  }
+
+  // Optimization 1: Disable Nagle's algorithm on the sending socket
+  int opt_nodelay = 1;
+  if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &opt_nodelay, sizeof(opt_nodelay)) < 0) {
+    log_error("setsockopt(TCP_NODELAY) failed:", errno);
+    close(sock);
     return;
   }
 
